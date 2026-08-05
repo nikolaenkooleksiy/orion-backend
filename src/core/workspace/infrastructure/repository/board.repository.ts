500e2d37836290330cf2398b0bc4b9ae536a3e3f
nikolaenko <aleshka.dev@gmail.com>
2026-08-05 17:35:13 +0300

    refactor(board): remove console log from create method in BoardRepository

--- src/core/workspace/infrastructure/repository/board.repository.ts
+++ src/core/workspace/infrastructure/repository/board.repository.ts
@@ -19,8 +19,6 @@ export class BoardRepository implements IBoardRepository {
   async create(board: BoardModel): Promise<BoardModel> {
     const data = BoardMapper.toPersistence(board);
 
-    console.log('Creating board with data:', board);
-
     const newBoard = await this.db.board.create({
       data: {
         ...data,
